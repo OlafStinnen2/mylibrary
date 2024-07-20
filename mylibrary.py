23308d6bdab6b90d0aa3c51bb29c98bab7c1794a
OlafStinnen2 <82947177+OlafStinnen2@users.noreply.github.com>
2024-07-20 14:37:00 +0000

    Add content from day 80

--- mylibrary.py
+++ mylibrary.py
@@ -1041,7 +1041,7 @@ def cool_form():
 
 app.run(host='0.0.0.0', port=81)
 
-## Simple html file that uses forms to collect user input
+## Simple example.html file that uses forms to collect user input
 ## you need to rename the form.html page into index.html page to run it in replit
 <!DOCTYPE html><!-- Document type declaration -->
 <html><!-- Root element -->
@@ -1086,3 +1086,28 @@ app.run(host='0.0.0.0', port=81)
 </body>
 
 </html>
+
+#This program collects data from a "form" section in "example.html" and sends it to a dictionary "form" via POST method and transfers data from the "form" section into the "form" dictionary via "request"-method
+#Depending what has been selected as option value in example.html the programe will display a different message
+  
+from flask import Flask, request, render_template  # Import necessary modules from Flask
+
+app = Flask(__name__)  # Create an instance of the Flask class
+
+@app.route("/login", methods=["POST"])  # Define route for '/login' with POST method
+def process():  # Define the function process to handle the '/login' route
+  page = ""  # Initialize an empty string variable or an empty page
+  form = request.form  # Get the form data from the POST request
+
+  if form["baldies"] == "david":  # Check if the 'baldies' field in the form equals 'david'
+    page += f"You're alright {form['username']}"  # Append a success message to the page variable
+  else:
+    page += f"You've picked wrong {form['username']}"  # Append a failure message to the page variable
+  return page  # Return the final page and display the message
+
+
+@app.route('/')  # Define route for the root URL
+def index():  # Define the function index to handle the root URL
+  return render_template('example.html')  # Render the 'example.html' template
+
+app.run(host='0.0.0.0', port=81)  # Run the Flask application on host '0.0.0.0' and port 81
